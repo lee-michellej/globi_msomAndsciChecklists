8d843e71ee8d37691149e8b9e3e8fe25fa94551d
mjasminelee <michellejlee@ucsb.edu>
2021-12-15 10:38:41 -0600

    added new file for plant standardization, new institution code list

--- Code/InstitutionCodeMatching/MatchSourceWithCodes.R
+++ Code/InstitutionCodeMatching/MatchSourceWithCodes.R
@@ -13,12 +13,7 @@
 
 library(tidyverse)
 
-setwd("~/Desktop/globi_bees/Data")
-
-
-
-
-
+setwd("~/Downloads")
 
 
 # 1 - open a subset of globi data set
@@ -26,12 +21,59 @@ setwd("~/Desktop/globi_bees/Data")
 ### remove spaces
 ### separate institution from catalog number
 
-interactiondata <- read_csv("globi_data_subset_bee_plant_2021_05_14.csv") %>%
-  select(sourceCatalogNumber) %>% 
-  mutate(., fullcode = sub(" ","",interactiondata$sourceCatalogNumber))
 
-interactiondata %>% 
-  mutate(., withdashcode = sub("_","-",interactiondata$fullcode)) %>% 
+#sourceInstitutionCode (shorter version)
+interactiondata <- read_tsv("all_bee_data_unique.txt")
+
+# to view a smaller subset
+subsetdata <- interactiondata[sample(1:nrow(interactiondata), 50,
+                                     replace=FALSE),]
+
+
+
+
+
+
+
+
+
+
+# sourceInstitionCode GLOBI COLUMN
+
+#select just institution code:
+globiinstitutioncode <- interactiondata %>% 
+  select(sourceInstitutionCode) %>% 
+  rename(., code = sourceInstitutionCode)
+
+subsetglobiinstitutioncode <- globiinstitutioncode[sample(1:nrow(globiinstitutioncode), 50, replace=FALSE),]
+# doesn't seem to be any dashes needed
+# might need to break off institutioncode from SCAN
+
+
+
+
+
+
+
+# sourceCatalogNumber GLOBI COLUMN
+
+#select just catalog number:
+globicatalognumber <- interactiondata %>% 
+  select(sourceCatalogNumber) %>% 
+  mutate(., fullcode = sub(" ","",globicatalognumber$sourceCatalogNumber))
+globicatalognumber_edit <- globicatalognumber %>% 
+  mutate(., withdashcode = sub("_","-",globicatalognumber$fullcode)) %>% 
+  separate(withdashcode, 
+           into = c("code", "num"), 
+           sep = "(?<=[A-Za-z])(?=[0-9])"
+  )
+
+
+# test above code with subset of data
+subsetglobicatalognumber <- globicatalognumber[sample(1:nrow(globicatalognumber), 50, replace=FALSE),]
+subsetglobicatalognumber <- subsetglobicatalognumber %>% 
+  mutate(., fullcode = sub(" ","",subsetglobicatalognumber$sourceCatalogNumber)) %>% 
+  mutate(., withdashcode = sub("_","-",subsetglobicatalognumber$fullcode)) %>% 
   separate(withdashcode, 
            into = c("code", "num"), 
            sep = "(?<=[A-Za-z])(?=[0-9])"
@@ -42,28 +84,86 @@ interactiondata %>%
 
 # 2 - open institution list taken and cleaned from https://scan-bugs.org/portal/collections/index.php
 
-institution_codes <- read_csv("institutioncodes_oct21.csv")
+setwd("~/Desktop/globi_bees/Data")
+
+institution_codes <- read_csv("institutioncodes_oct21.csv") %>%
+  separate(code, 
+           into = c("precode", "postcode"), 
+           sep = "-",
+           remove = FALSE)
 
 
 
 
 
 
-# 3 - join tables
 
-fulldf <- left_join(interactiondata, institution_codes, by = "code")
+# 3 - join with institution code
 
-
+institutionCodes <- left_join(globiinstitutioncode, institution_codes, by = "code", keep = TRUE) %>% 
+  filter(code.x != "NA") %>% 
+  filter(is.na(code.y))
 
 
 
 # 4 - check for unmatched institutions 
 
-# some of the unmatched institution codes:
-### BBSLID, KWC, HOLO, LACMENT, PUB
+list_needcode <- as.data.frame(unique(institutionCodes$code.x))
 
 
 
 
 
 
+# 5 - join with catalog number
+
+catalogNumber <- left_join(globicatalognumber, institution_codes, by = "code", keep = TRUE) %>% 
+  filter(code.x != "NA") %>% 
+  filter(is.na(code.y))
+
+
+
+
+
+
+# retry for a work flow matching by institutioncode and then catalognumbercutup
+
+sourceLists <- interactiondata %>% 
+  select(sourceInstitutionCode, sourceCatalogNumber)%>% 
+  mutate(., fullcode = sub(" ","",sourceLists$sourceCatalogNumber))
+sourceLists_edit <- sourceLists %>% 
+  mutate(., withdashcode = sub("_","-",sourceLists$fullcode)) %>% 
+  separate(withdashcode, 
+           into = c("lettercode", "num"), 
+           sep = "(?<=[A-Za-z])(?=[0-9])"
+  ) %>% 
+  filter(sourceInstitutionCode != "NA")
+
+
+# will need to merge separately and then re-merge
+
+df1_institutioncode <- anti_join(sourceLists_edit, institution_codes, by = c("sourceInstitutionCode" = "code"))
+
+df2_institutioncode <- anti_join(df1_institutioncode, institution_codes, by = c("sourceInstitutionCode" = "precode"), keep = TRUE)
+
+df3_instcode <- anti_join(df2_institutioncode, institution_codes, by = c("lettercode" = "code"), keep = TRUE)
+
+df4 <- anti_join(df3_instcode, institution_codes, by = c("lettercode" = "precode"))
+
+
+neednames <- data.frame("neednames" = unique(df4$sourceInstitutionCode))
+
+df5 <- anti_join(neednames, institution_codes, by = c("neednames" = "precode"))
+
+
+setwd("~/Desktop")
+write.csv(neednames, "institutions_neednames.csv", row.names = FALSE)
+
+
+
+### locate list with codes with not much institutiondata
+locate1 <- filter(interactiondata, sourceInstitutionCode == "F")
+locate2 <- filter(interactiondata, sourceInstitutionCode == "US")
+locate <- rbind(locate1, locate2)
+setwd("~/Downloads")
+write.csv(locate, "FandUScodes.csv", row.names = FALSE)
