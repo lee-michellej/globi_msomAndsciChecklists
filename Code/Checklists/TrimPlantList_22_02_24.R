d91df0cb6167a44829d6e1e9789cfd6855aba35e
mjasminelee <michellejlee@ucsb.edu>
2022-02-24 16:14:12 -0800

    added some items to files, creating new plant trimming code, meeting notes edits

---
+++ Code/Checklists/TrimPlantList_22_02_24.R
@@ -0,0 +1,8 @@
+# Temporarily trim plant list for model troubleshooting #
+# created by mjlee 24 feb 22
+
+# the purpose of this code is to find candidate plant families to trim. the goal is that this list shortening will be temporary, so the model will take less time to run. trimming this list will also be biologically based.
+
+
+# load libraries and data files
+# match family list with the list of plants that are part of the final globi dataset
