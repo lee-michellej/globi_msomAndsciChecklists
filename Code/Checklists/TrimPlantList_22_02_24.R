3bad70486e8d5922c3dcde50af2113a13c8f7414
mjasminelee <michellejlee@ucsb.edu>
2022-04-11 09:54:50 -0700

    update to plant checklist trimming and meeting notes. 11apr22

--- Code/Checklists/TrimPlantList_22_02_24.R
+++ Code/Checklists/TrimPlantList_22_02_24.R
@@ -90,3 +90,67 @@ write.csv(sci.shortened.final, "short_resolvedplantsci_022422.csv", row.names = 
 
 
 
+
+# without apis | make shortened list ---------------
+
+
+globi.apis.list <- dat.readin %>% 
+  select(targetTaxonFamilyName, sourceTaxonGenusName) %>% 
+  rename(Family = targetTaxonFamilyName,
+         beeGenus = sourceTaxonGenusName) %>% 
+  filter(beeGenus != "Apis") %>% 
+  select(Family)
+# cleaned list of 566 interactions from globi for the dataset
+# cutting out Apis brings list to 412 interactions
+
+unique.noapis.list <- unique(globi.apis.list)
+# 26 unique plant families in the cleaned list
+
+
+
+
+globi.noapis.famgen.list <- dat.readin %>% 
+  select(targetTaxonFamilyName, 
+         targetTaxonGenusName, 
+         sourceTaxonGenusName) %>% 
+  rename(Family = targetTaxonFamilyName,
+         Genus = targetTaxonGenusName,
+         beeGenus = sourceTaxonGenusName) %>% 
+  mutate(fam.gen = paste(Family, Genus, sep = '.')) %>% 
+  filter(beeGenus != "Apis") %>% 
+  select(-beeGenus)
+
+
+unique.noapis.fam.gen <- unique(globi.noapis.famgen.list)
+# 59 family genus combinations
+
+
+
+
+
+# without apis | match lists by family ------
+# match family list with the list of plants that are part of the no apis globi interaction dataset
+
+
+filtered.byFam <- filter(sci.fam, 
+                         sci.fam$Family %in% unique.noapis.list$Family)
+# 341 unique plant entries which cuts down the overall list considerably
+# however, the goal is to cut it down even more than this
+
+
+
+
+
+# without apis | match lists by family & genus ------
+filtered.byGen <- filter(sci.famgen, 
+                         sci.famgen$fam.gen %in% unique.noapis.fam.gen$fam.gen)
+# this filter down to 126 plants
+
+
+
+# print data file of shortened plants without apis (126) ------------
+
+sci.shortened.final.noapis <- filtered.byGen %>% 
+  select(-fam.gen)
+
+write.csv(sci.shortened.final.noapis, "short_noapis_resolvedplantsci_022422.csv", row.names = FALSE)
