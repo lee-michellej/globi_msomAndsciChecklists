5e6ef52a6b0dbba2a5cb0fc9e7d036978e86748d
Grace <grace.direnzo@gmail.com>
2025-02-25 14:27:00 -0500

    update code for HPC

---
+++ Code/Workflow/6 - Model run HPC/_loop-shell-script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define an array of model names
+models=("no_bee_plant" "bee_species" "plant_species" "bee_family" "plant_family" "bee_plant_family")
+
+# Define model run type: either test or full
+model_run_type="full"
+
+# Loop through each model name
+for model in "${models[@]}"; do
+    echo "Submitting job for model: $model with run type: $model_run_type"
+
+    # Submit the job with a unique job name and pass environment variables
+    sbatch --job-name="globi-${model}" --export=MODEL_NAME="$model",MODEL_RUN_TYPE="$model_run_type" /home/gdirenzo/globi/Code/globi-job.sh
+
+    echo "Job submitted for model: $model"
+done
+
+echo "All jobs have been submitted."
